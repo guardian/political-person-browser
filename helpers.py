b2ba9edc2e4f24344a538a265d88e22ebd3132c1
Michael Brunton-Spall <michael.brunton-spall@guardian.co.uk>
2010-10-07 15:51:33 +0100

    Load up lists of MP's

--- helpers.py
+++ helpers.py
@@ -1,6 +1,27 @@
 import os
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
+from google.appengine.api.urlfetch import fetch
+from google.appengine.api.labs import taskqueue
+
+import logging
+
+try:
+    import json
+except ImportError:
+    from django.utils import json
+
+def load_from_json_endpoint(url):
+    logging.info('Requesting "%s"' % (url))
+    response = fetch(url)
+    logging.info('Got "%d"' % (response.status_code))
+    if response.status_code == 200:
+        return json.loads(response.content)
+    return None
+
+def add_task(queue_name, url, params):
+    logging.info('Adding task for "%s" with params "%s"' % (url, params))
+    taskqueue.add(queue_name=queue_name, url=url, params=params)
 
 def render_admin_template(self, end_point, template_values):
     user = users.get_current_user()
--- main.py
+++ main.py
@@ -19,9 +19,27 @@ class MainHandler(RequestHandler):
         logging.info("Got request for /")
         helpers.render_template(self, 'index.html', {})
 
+class LoadMPHandler(RequestHandler):
+    def post(self):
+        id = self.request.get('id')
+        api_url = self.request.get('api_url')
+        json = helpers.load_from_json_endpoint(api_url)
+        #Do Something with the returned information
+        logging.info('Got information on MP %s from %s' % (json['person']['name'], api_url))
+
+class LoadFeedHandler(RequestHandler):
+    def get(self):
+        json = helpers.load_from_json_endpoint('http://www.guardian.co.uk/politics/api/general-election/2010/results/json')
+        for constituency in json['results']['called-constituencies']:
+            mp_id = constituency['result']['winning-mp']['aristotle-id']
+            api_url = constituency['result']['winning-mp']['json-url']
+            helpers.add_task('feed', '/tasks/loadmp', {'id':mp_id, 'api_url':api_url})
+
 def main():
     application = WSGIApplication([
         ('/', MainHandler),
+        ('/feeds/import', LoadFeedHandler),
+        ('/tasks/loadmp', LoadMPHandler),
     ],
     debug=True)
     run_wsgi_app(application)
